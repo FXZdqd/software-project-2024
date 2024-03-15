cedf1c3c9e44a6d717d5f97f992ad296418c5e14
Tammy <2822429829@qq.com>
2024-03-15 16:05:23 +0800

    home 3-15 16:00

--- InsightSpace_WxMiniProgram/src/pages/index/index.vue
+++ InsightSpace_WxMiniProgram/src/pages/index/index.vue
@@ -3,10 +3,69 @@ import CustomNavBar from './components/CustomNavBar.vue'
 </script>
 
 <template>
-  <CustomNavBar />
-  <view class="index">index</view>
+  <view class="index">
+    <swiper class="banner">
+      <swiper-item>
+        <image src="@/static/images/ad1.png"></image>
+      </swiper-item>
+    </swiper>
+
+    <uni-search-bar :focus="true"> </uni-search-bar>
+    <uni-card
+      title="问题"
+      sub-title="标签"
+      extra="2024-3-15"
+      thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+    >
+      <text>内容内容内容内容内容内容内容内容内容内容</text>
+    </uni-card>
+    <uni-card
+      title="问题"
+      sub-title="标签"
+      extra="2024-3-15"
+      thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+    >
+      <text>内容内容内容内容内容内容内容内容内容内容</text>
+    </uni-card>
+    <uni-card
+      title="问题"
+      sub-title="标签"
+      extra="2024-3-15"
+      thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+    >
+      <text>内容内容内容内容内容内容内容内容内容内容</text>
+    </uni-card>
+    <uni-card
+      title="问题"
+      sub-title="标签"
+      extra="2024-3-15"
+      thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+    >
+      <text>内容内容内容内容内容内容内容内容内容内容</text>
+    </uni-card>
+    <uni-card
+      title="问题"
+      sub-title="标签"
+      extra="2024-3-15"
+      thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+    >
+      <text>内容内容内容内容内容内容内容内容内容内容</text>
+    </uni-card>
+    <uni-card
+      title="问题"
+      sub-title="标签"
+      extra="2024-3-15"
+      thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+    >
+      <text>内容内容内容内容内容内容内容内容内容内容</text>
+    </uni-card>
+  </view>
 </template>
 
 <style lang="scss">
-//
+.banner,
+.banner image {
+  width: 450px;
+  height: 200px;
+}
 </style>
